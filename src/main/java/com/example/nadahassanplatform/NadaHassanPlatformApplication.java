52044de91adb85d592addfb8898c0a434a6d7677
m.fouad_95@hotmail.com <Mostafa-fouad>
2022-12-03 02:09:16 +0200

    Project Creation

---
+++ src/main/java/com/example/nadahassanplatform/NadaHassanPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.nadahassanplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadaHassanPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadaHassanPlatformApplication.class, args);
+	}
+
+}
