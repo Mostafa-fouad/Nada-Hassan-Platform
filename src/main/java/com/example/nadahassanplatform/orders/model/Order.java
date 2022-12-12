e11daea43380371de854eb1d17c5781f2b3dce32
Mostafa-fouad <m.fouad_95@hotmail.com>
2022-12-12 22:31:16 +0200

    fix database relations

--- src/main/java/com/example/nadahassanplatform/orders/model/Order.java
+++ src/main/java/com/example/nadahassanplatform/orders/model/Order.java
@@ -11,7 +11,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.Instant;
-import java.util.Set;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -27,11 +27,8 @@ public class Order {
     private static final String ORDER_SUBMISSION_ID_COLUMN_NAME = "order_submission_id";
     private static final String ADDRESS_COLUMN_NAME = "address";
     private static final String CUSTOMER_MOBILE_COLUMN_NAME = "customer_mobile";
-    private static final String ORDER_ITEMS_COLUMN_NAME = "order_items";
     private static final String CREATION_DATE_COLUMN_NAME = "created_date";
     private static final String UPDATED_DATE_COLUMN_NAME = "updated_date";
-
-    private static final String ORDER_ID_COLUMN_NAME = "order_id";
 
     static final String JSON_B_TYPE = "jsonb";
     static final String TABLE_NAME = "order";
@@ -50,10 +47,7 @@ public class Order {
     private String customerMobile;
 
     @Type(type = JSON_B_TYPE)
-    @Column(name = ORDER_ITEMS_COLUMN_NAME, nullable = false)
-    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
-    @ToString.Exclude
-    private Set<Product> orderItems;
+    private List<Product> orderItems;
 
     @CreationTimestamp
     @Column(name = CREATION_DATE_COLUMN_NAME, nullable = false)
